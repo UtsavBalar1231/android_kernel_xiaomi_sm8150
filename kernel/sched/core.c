ca4744cefdd4a3480c8addde7fa092bfb41f1d62
Sami Tolvanen <samitolvanen@google.com>
2018-05-02 10:44:59 -0700

    ANDROID: add support for clang Shadow Call Stack (SCS)
    This change adds generic support for clang's Shadow Call Stack, which
    uses a shadow stack to protect return addresses from being overwritten
    by an attacker. Details are available here:
    
      https://clang.llvm.org/docs/ShadowCallStack.html
    
    Bug: 112277034
    Change-Id: Idd553b7c978b0673ab533a68980fb9a654f4510c
    Signed-off-by: Sami Tolvanen <samitolvanen@google.com>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -30,6 +30,7 @@
 #include <linux/delay.h>
 
 #include <linux/kthread.h>
+#include <linux/scs.h>
 
 #include <asm/switch_to.h>
 #include <asm/tlb.h>
@@ -5506,6 +5507,8 @@ void init_idle(struct task_struct *idle, int cpu)
 {
 	struct rq *rq = cpu_rq(cpu);
 	unsigned long flags;
+
+	scs_task_reset(idle);
 
 	__sched_fork(0, idle);
 
