9aa77482a19e8cbc0c40fb162fca51f058bfb504
YueHaibing <yuehaibing@huawei.com>
2019-01-04 01:38:29 +0000

    f2fs: remove set but not used variable 'err'
    Fixes gcc '-Wunused-but-set-variable' warning:
    
    fs/f2fs/data.c: In function 'f2fs_dio_submit_bio':
    fs/f2fs/data.c:2585:6: warning:
     variable 'err' set but not used [-Wunused-but-set-variable]
    
    Signed-off-by: YueHaibing <yuehaibing@huawei.com>
    Reviewed-by: Chao Yu <yuchao0@huawei.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    Signed-off-by: UtsavisGreat <utsavbalar1231@gmail.com>

--- fs/f2fs/data.c
+++ fs/f2fs/data.c
@@ -2473,8 +2473,7 @@ static int check_direct_IO(struct inode *inode, struct iov_iter *iter,
 
 	if (offset & blocksize_mask)
 		return -EINVAL;
-
-	if (iov_iter_alignment(iter) & blocksize_mask)
+		if (iov_iter_alignment(iter) & blocksize_mask)
 		return -EINVAL;
 
 	return 0;
