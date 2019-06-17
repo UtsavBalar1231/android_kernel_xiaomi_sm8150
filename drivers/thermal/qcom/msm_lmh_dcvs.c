32ca8cb350b60612a61186bc4c036ba390bd6ab9
Manaf Meethalavalappu Pallikunhi <manafm@codeaurora.org>
2019-06-17 17:33:08 +0530

    drivers: lmh_dcvs: Add support to selectively register cooling devices
    CPU cooling device register and handling max and min limit
    functionalities are moved to cpu clock driver and cpu vdd min voltage
    cooling device driver respectively. Add support to selectively
    register cooling devices for LMH DCVSh driver so that other LMH DCVSh
    features can be enabled without cooling device register.
    
    Change-Id: I9e346c2410ff34c5b1dc9d0fdc19970a403007cc
    Signed-off-by: Manaf Meethalavalappu Pallikunhi <manafm@codeaurora.org>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/thermal/qcom/msm_lmh_dcvs.c
+++ drivers/thermal/qcom/msm_lmh_dcvs.c
@@ -560,6 +560,7 @@ static int limits_dcvs_probe(struct platform_device *pdev)
 	cpumask_t mask = { CPU_BITS_NONE };
 	const __be32 *addr;
 	unsigned long max_limit = 0;
+	bool no_cdev_register = false;
 
 	for_each_possible_cpu(cpu) {
 		cpu_node = of_cpu_device_node_get(cpu);
@@ -619,6 +620,9 @@ static int limits_dcvs_probe(struct platform_device *pdev)
 	/* Check whether platform mitigation needs to enable or not */
 	hw->is_plat_mit_disabled = of_property_read_bool(dn,
 				"qcom,plat-mitigation-disable");
+
+	no_cdev_register = of_property_read_bool(dn,
+				"qcom,no-cooling-device-register");
 
 	addr = of_get_address(dn, 0, NULL, NULL);
 	if (!addr) {
@@ -706,11 +710,14 @@ probe_exit:
 	mutex_unlock(&lmh_dcvs_list_access);
 	lmh_debug_register(pdev);
 
-	ret = cpuhp_setup_state(CPUHP_AP_ONLINE_DYN, "lmh-dcvs/cdev:online",
-				limits_cpu_online, NULL);
-	if (ret < 0)
-		goto unregister_sensor;
-	ret = 0;
+	if (!no_cdev_register) {
+		ret = cpuhp_setup_state(CPUHP_AP_ONLINE_DYN,
+					"lmh-dcvs/cdev:online",
+					limits_cpu_online, NULL);
+		if (ret < 0)
+			goto unregister_sensor;
+		ret = 0;
+	}
 
 	return ret;
 
