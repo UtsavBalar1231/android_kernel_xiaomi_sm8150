85c67b36dde646ded3cdc4e5b5e268ff002994e9
UtsavisGreat <utsavbalar1231@gmail.com>
2019-08-29 17:45:46 +0000

    exec: dont blacklist powerhal service
    Signed-off-by: UtsavisGreat <utsavbalar1231@gmail.com>

--- include/linux/binfmts.h
+++ include/linux/binfmts.h
@@ -154,7 +154,6 @@ static inline bool task_is_booster(struct task_struct *tsk)
 
 	get_task_comm(comm, tsk);
 	return !strcmp(comm, "init") || !strcmp(comm, "NodeLooperThrea") ||
-	       !strcmp(comm, "power@1.3-servi") ||
 	       !strcmp(comm, "perf@1.0-servic") ||
 	       !strcmp(comm, "init.qcom.post_");
 }
