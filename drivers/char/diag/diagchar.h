b3feebcd9ccb94ead8799654c074f7621f48e75e
UtsavBalar1231 <utsavbalar1231@gmail.com>
2020-05-15 22:19:45 +0530

    Revert "diag: Correct the transport query command code"
    This reverts commit 70375410c8522c00eb01cf4fa0735c7cd988711d.

--- drivers/char/diag/diagchar.h
+++ drivers/char/diag/diagchar.h
@@ -142,7 +142,6 @@
 #define DIAG_GET_TIME_API	0x21B
 #define DIAG_SET_TIME_API	0x21C
 #define DIAG_GET_DIAG_ID	0x222
-#define DIAG_QUERY_TRANSPORT	0x223
 #define DIAG_SWITCH_COMMAND	0x081B
 #define DIAG_BUFFERING_MODE	0x080C
 
@@ -318,10 +317,8 @@ do {						\
 #define DIAG_ID_UNKNOWN		0
 #define DIAG_ID_APPS		1
 
-#define DIAG_ROUTE_TO_UART	1
-#define DIAG_ROUTE_TO_USB	2
-#define DIAG_ROUTE_TO_PCIE	3
-
+#define DIAG_ROUTE_TO_USB 0
+#define DIAG_ROUTE_TO_PCIE 1
 /* List of remote processor supported */
 enum remote_procs {
 	MDM = 1,
